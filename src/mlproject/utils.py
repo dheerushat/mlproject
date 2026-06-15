83adee489947934bdfd5e932cd95cfb57bbda9f4
dheerusha tiwari <dheerushat@gmail.com>
2026-06-14 21:08:15 -0500

    Model training

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -4,18 +4,21 @@ from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 import pandas as pd
 from dotenv import load_dotenv
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 import pymysql
 
 import pickle
 import numpy as np
-
 
 load_dotenv()
 
 host=os.getenv("host")
 user=os.getenv("user")
 password=os.getenv("password")
-db=os.getenv("db")
+db=os.getenv('db')
+
+
 
 def read_sql_data():
     logging.info("Reading SQL database started")
@@ -24,18 +27,19 @@ def read_sql_data():
             host=host,
             user=user,
             password=password,
-            db=db,
-            port=int(os.getenv("port"))
+            db=db
         )
-        logging.info("Connection established" ,mydb)
-        df=pd.read_sql_query('SELECT * FROM students', mydb)
+        logging.info("Connection Established",mydb)
+        df=pd.read_sql_query('Select * from students',mydb)
         print(df.head())
 
         return df
-    
+
+
+
     except Exception as ex:
         raise CustomException(ex)
-
+    
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -48,4 +52,24 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
 
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
